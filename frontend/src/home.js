9543452c47f23430c3f66f32e9901c4e9c5d3316
Neel14-stack <nilmr45@gmail.com>
2022-11-09 00:12:08 +0530

    changes made in frontend

--- frontend/src/home.js
+++ frontend/src/home.js
@@ -213,7 +213,7 @@ export const ImageUpload = () => {
       <AppBar position="static" className={classes.appbar}>
         <Toolbar>
           <Typography className={classes.title} variant="h6" noWrap>
-            CodeBasics: Potato Disease Classification
+            Rec Shokubutsu: A Plant Identification Application
           </Typography>
           <div className={classes.grow} />
           <Avatar src={cblogo}></Avatar>
